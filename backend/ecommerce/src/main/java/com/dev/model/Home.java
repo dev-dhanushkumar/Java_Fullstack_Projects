e72716fa834bbede09e4080c2afc43ac18d02533
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-19 00:09:32 +0530

    [feat]: Add needed entity and models 🎨

---
+++ backend/ecommerce/src/main/java/com/dev/model/Home.java
@@ -0,0 +1,18 @@
+package com.dev.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Home {
+    private List<HomeCategory> grid;
+
+    private List<HomeCategory> shopByCategories;
+
+    private List<HomeCategory> electricCategories;
+
+    private List<HomeCategory> dealCategories;
+
+    private List<Deal> deals;
+}
---
+++ backend/ecommerce/src/main/java/com/dev/model/PaymentDetails.java
@@ -0,0 +1,15 @@
+package com.dev.model;
+
+import com.dev.domain.PaymentStatus;
+
+import lombok.Data;
+
+@Data
+public class PaymentDetails {
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayPaymentLinkReferenceId;
+    private String razorpayPaymentLinkStatus;
+    private String razorpayPaymentIdZWSP;
+    private PaymentStatus status;
+}
