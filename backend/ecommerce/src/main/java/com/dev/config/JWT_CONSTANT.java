9dcfe650b9229cf42be0a79914d5bea8919507a3
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-19 17:57:14 +0530

    [config]: secret Key here

---
+++ backend/ecommerce/src/main/java/com/dev/config/JWT_CONSTANT.java
@@ -0,0 +1,7 @@
+package com.dev.config;
+
+public class JWT_CONSTANT {
+    public static final String SECRET_KEY = "kljncliuehr89u4riufn34yf2i3p948r3r0f[93[09jgr]]";
+    public static final String JWT_HEADER = "Authorization";
+
+}
