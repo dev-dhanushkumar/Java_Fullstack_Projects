c2900d9167156241d1778eb0fe0dc265a5a727ef
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-19 17:58:49 +0530

    [Controller]:Authentication Controller API

---
+++ backend/ecommerce/src/main/java/com/dev/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.dev.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.domain.USER_ROLE;
+import com.dev.response.AuthResponse;
+import com.dev.response.SignupRequest;
+import com.dev.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody SignupRequest req) {
+        String jwt = authService.createUser(req);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setMessage("register success");
+        res.setRole(USER_ROLE.ROLE_CUSTOMER);
+
+        return ResponseEntity.ok(res);
+    }
+}
