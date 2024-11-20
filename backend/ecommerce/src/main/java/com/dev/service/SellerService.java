880fcc94692912fb62805e4421ed03aa29840527
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-20 20:49:14 +0530

    [@Service]: Seller CRUD  Service Here

---
+++ backend/ecommerce/src/main/java/com/dev/service/SellerService.java
@@ -0,0 +1,19 @@
+package com.dev.service;
+
+import java.util.List;
+
+import com.dev.domain.AccountStatus;
+import com.dev.model.Seller;
+
+public interface SellerService {
+    
+    Seller getSellerProfile(String jwt) throws Exception;
+    Seller createSeller(Seller seller) throws Exception;
+    Seller getSellerById(Long id) throws Exception;
+    Seller getSellerByEmail(String email) throws Exception;
+    List<Seller> getAllSellers(AccountStatus status);
+    Seller updateSeller(Long id, Seller seller) throws Exception;
+    void deleteSeller(Long id) throws Exception;
+    Seller verifyEmail(String email, String otp) throws Exception;
+    Seller updateSellerAccountStatus(Long id, AccountStatus status) throws Exception;
+}
