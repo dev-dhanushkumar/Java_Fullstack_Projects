4da2acbaece88f5f1d31ab9a3af87b43c6bc2576
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-20 20:42:51 +0530

    [fix]: Fix the Aauthentication otp bug

--- backend/ecommerce/src/main/java/com/dev/controller/AuthController.java
+++ backend/ecommerce/src/main/java/com/dev/controller/AuthController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.domain.USER_ROLE;
+import com.dev.request.LoginOtp;
+import com.dev.request.LoginRequest;
+import com.dev.response.ApiResponse;
 import com.dev.response.AuthResponse;
 import com.dev.response.SignupRequest;
 import com.dev.service.AuthService;
@@ -30,5 +33,23 @@ public class AuthController {
         res.setRole(USER_ROLE.ROLE_CUSTOMER);
 
         return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/send/login-signup-otp")
+    public ResponseEntity<ApiResponse> sendOtpHandler(@RequestBody LoginOtp req) throws Exception {
+        authService.sendLoginOtp(req.getEmail(), req.getRole());
+
+        ApiResponse res = new ApiResponse();
+        
+        res.setMessage("otp send successfully");
+
+        return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> loginHandler(@RequestBody LoginRequest req) throws Exception {
+        AuthResponse authResponse = authService.signIn(req);
+
+        return ResponseEntity.ok(authResponse);
     }
 }
