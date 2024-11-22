a6c6be2c9986dd4d0b694af4ab38af1ac30c1447
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-22 15:04:16 +0530

    refactor: Update SellerService to integrate with newly added services

--- backend/ecommerce/src/main/java/com/dev/service/SellerService.java
+++ backend/ecommerce/src/main/java/com/dev/service/SellerService.java
@@ -3,13 +3,14 @@ package com.dev.service;
 import java.util.List;
 
 import com.dev.domain.AccountStatus;
+import com.dev.exceptions.SellerException;
 import com.dev.model.Seller;
 
 public interface SellerService {
     
     Seller getSellerProfile(String jwt) throws Exception;
     Seller createSeller(Seller seller) throws Exception;
-    Seller getSellerById(Long id) throws Exception;
+    Seller getSellerById(Long id) throws SellerException;
     Seller getSellerByEmail(String email) throws Exception;
     List<Seller> getAllSellers(AccountStatus status);
     Seller updateSeller(Long id, Seller seller) throws Exception;
