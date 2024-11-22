d4baf6532d6afdcfe69fce9c50b8f7e60df0da2c
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-22 15:10:08 +0530

    [Adds]: create Review JSON Body Request

---
+++ backend/ecommerce/src/main/java/com/dev/request/CreateReviewRequest.java
@@ -0,0 +1,12 @@
+package com.dev.request;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CreateReviewRequest {
+    private String reviewText;
+    private double reviewRating;
+    private List<String> productImages;
+}
