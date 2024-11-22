994cea3e562090ae462dd626111f827bcabccf31
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-11-22 14:41:17 +0530

    [Exception]:Product and Seller Exception

---
+++ backend/ecommerce/src/main/java/com/dev/exceptions/ProductException.java
@@ -0,0 +1,7 @@
+package com.dev.exceptions;
+
+public class ProductException extends Exception{
+    public ProductException(String message) {
+        super(message);
+    }
+}
---
+++ backend/ecommerce/src/main/java/com/dev/exceptions/SellerException.java
@@ -0,0 +1,7 @@
+package com.dev.exceptions;
+
+public class SellerException  extends Exception{
+    public SellerException(String message) {
+        super(message);
+    }
+}
