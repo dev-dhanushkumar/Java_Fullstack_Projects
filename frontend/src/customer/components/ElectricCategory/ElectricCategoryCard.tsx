479366cdcc2cf1f53a53455c08f2b8bd3be65a75
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-12-03 20:47:34 +0530

    Create ElectricCategoryCard for electric category items

---
+++ frontend/src/customer/components/ElectricCategory/ElectricCategoryCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ElectricCategoryCard = () => {
+  return (
+    <div>
+        <img className='object-contain h-10' src="https://rukminim2.flixcart.com/image/312/312/xif0q/computer/8/m/i/-original-imah5f39ed4r5hfr.jpeg?q=70" alt=""  />
+        <h2 className='font-semibold text-sm'>Laptop</h2>
+    </div>
+  )
+}
+
+export default ElectricCategoryCard
