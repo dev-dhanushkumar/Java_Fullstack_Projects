1a8e82c67176ca1593a33baa0a4f06961ce17704
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2024-12-13 08:16:00 +0530

    [refactor]:Add hover navebar category

--- frontend/src/customer/components/Navbar/Navbar.tsx
+++ frontend/src/customer/components/Navbar/Navbar.tsx
@@ -4,13 +4,18 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 // import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { AddShoppingCart, FavoriteBorder, Storefront } from "@mui/icons-material";
+import CategorySheet from "./CategorySheet";
+import { mainCategory } from "../../../data/category/mainCategory";
+import { useState } from "react";
 
 function Navbar() {
     const theme = useTheme();
     const isLarge = useMediaQuery(theme.breakpoints.up("lg"));
+    const [selectedCategory, setSelectedCategory] = useState("men");
+    const [showCategory, setShowCategory] = useState(false);
   return (
     <div>
-      <Box>
+      <Box className="sticky top-0 left-0 right-0 bg-white" sx={{zIndex:2}}>
         <div className="flex items-center justify-between px-5 lg:px-20 h-[70px] border-b">
           <div className="flex items-center gap-9">
             <div className="flex items-center gap-2">
@@ -22,8 +27,15 @@ function Navbar() {
               </h1>
             </div>
             <ul className="flex items-center font-medium text-gray-800">
-                {["Men", "Women", "Home & Furniture", "Electronics"].map((item) => 
-                    <li className="mainCategory hover:text-primary-color hover:border-b-2 h-[70px] px-4 border-primary-color flex items-center">{item}</li>
+                {mainCategory.map((item) => 
+                    <li onMouseLeave={() => {
+                      setShowCategory(false);
+                    }}
+                    onMouseEnter={()=> {
+                      setShowCategory(true);
+                      setSelectedCategory(item.categoryId);
+                    }}
+                    className="mainCategory hover:text-primary-color hover:border-b-2 h-[70px] px-4 border-primary-color flex items-center">{item.name}</li>
                 )}
             </ul>
           </div>
@@ -51,6 +63,14 @@ function Navbar() {
             </Button>}
           </div>
         </div>
+
+        
+        {showCategory && <div 
+        onMouseEnter={() => setShowCategory(true)}
+        onMouseLeave={() => setShowCategory(false)}
+        className="absolut top-[4.41rem] left-20 right-20 border">
+          <CategorySheet  selectedCategory={selectedCategory}/>
+        </div>}
       </Box>
     </div>
   );
